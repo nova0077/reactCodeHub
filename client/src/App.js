8408c97d08428202a4e18f529a875a134503c85a
Praveen <praveennooka123@gmail.com>
2023-10-22 13:18:57 +0530

    second commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost.js";
+import Post from "./pages/Post";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div className="navbar">   
+          <Link to="/createpost"> Create A Post</Link>
+          <Link to="/"> Home Page</Link>
+          <Link to="/login"> Login</Link>
+          <Link to="/registration">Register</Link>
+        </div>
+        <Routes>
+          <Route path="/" exact element={<Home />} /> 
+          <Route path="/createpost" exact element={<CreatePost/>} />
+          <Route path="/post/:id" exact element={<Post/>} />
+          <Route path="/login" exact element={<Login/>} />
+          <Route path="/registration" exact element={<Registration/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
